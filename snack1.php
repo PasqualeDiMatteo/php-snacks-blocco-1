0f16395ca340a8a2fb2f56e61832f833a9637ec2
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-07-17 14:19:21 +0200

    first commit

---
+++ snack1.php
@@ -0,0 +1,25 @@
+<?php
+
+$match_schedule = [
+    [
+        "home" => [
+            "name" => "frosinone",
+            "points" => 52
+        ],
+        "out_side" => [
+            "name" => "napoli",
+            "points" => 52
+        ]
+    ],
+    [
+        "home" => [
+            "name" => "genoa",
+            "points" => 52
+        ],
+        "out_side" => [
+            "name" => "fiorentina",
+            "points" => 52
+        ]
+    ],
+
+];
