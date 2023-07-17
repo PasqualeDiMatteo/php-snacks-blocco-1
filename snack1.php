97ef55d6a5e5c3545aa7a41010698ad8306b3008
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-07-17 14:39:22 +0200

    Added Snack 1

--- snack1.php
+++ snack1.php
@@ -4,22 +4,65 @@ $match_schedule = [
     [
         "home" => [
             "name" => "frosinone",
-            "points" => 52
+            "points" => 12
         ],
-        "out_side" => [
+        "guest" => [
             "name" => "napoli",
-            "points" => 52
+            "points" => 24
         ]
     ],
     [
         "home" => [
             "name" => "genoa",
-            "points" => 52
+            "points" => 22
         ],
-        "out_side" => [
+        "guest" => [
             "name" => "fiorentina",
-            "points" => 52
+            "points" => 18
         ]
     ],
-
+    [
+        "home" => [
+            "name" => "empoli",
+            "points" => 16
+        ],
+        "guest" => [
+            "name" => "verona",
+            "points" => 17
+        ]
+    ],
+    [
+        "home" => [
+            "name" => "inter",
+            "points" => 20
+        ],
+        "guest" => [
+            "name" => "monza",
+            "points" => 11
+        ]
+    ],
 ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 1</title>
+</head>
+
+<body>
+    <h1>Snack 1</h1>
+    <ul>
+        <?php foreach ($match_schedule as $match) : ?>
+            <li>
+                <?php echo ucfirst($match["home"]["name"]) ?> - <?php echo ucfirst($match["guest"]["name"]) ?> | <?php echo ucfirst($match["home"]["points"]) ?> - <?php echo ucfirst($match["guest"]["points"]) ?>
+            </li>
+        <?php endforeach ?>
+    </ul>
+</body>
+
+</html>
